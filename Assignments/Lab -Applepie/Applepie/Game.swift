c208302895a40aa39b07cc723e8d355d455f93aa
Varun <142438660+Varunsaini619@users.noreply.github.com>
2025-07-28 06:43:04 +0530

    Apple pie uploaded

---
+++ Assignments/Lab -Applepie/Applepie/Game.swift
@@ -0,0 +1,30 @@
+//
+//  Game.swift
+//  Applepie
+//
+//  Created by Varun Saini on 22/07/25.
+//
+
+
+
+import Foundation
+struct Game{
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters : [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord.append(letter)
+            } else {
+                guessedWord.append("_")
+            }
+        }
+        return guessedWord
+    }
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) { incorrectMovesRemaining -= 1 }
+    }
+}
