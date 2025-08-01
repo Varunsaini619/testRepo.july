eb817ed62571a1eca606f21d65e30c09bfc4d4f0
Varun <142438660+Varunsaini619@users.noreply.github.com>
2025-08-01 15:04:42 +0530

    Personality Quiz Project Uploaded
    All labs and assignments up to date

---
+++ Assignments and UI Projects/PersonalityQuizProject/PersonalityQuizProject/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuizProject
+//
+//  Created by Varun Saini on 31/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue){
+    }
+}
+
