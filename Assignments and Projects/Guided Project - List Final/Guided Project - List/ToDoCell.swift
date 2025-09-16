9e4e354ea5fc6ee44456a9a3d683c93efeb89a0c
Varun <142438660+Varunsaini619@users.noreply.github.com>
2025-09-16 11:51:46 +0530

    assignments uploaded till date

---
+++ Assignments and Projects/Guided Project - List Final/Guided Project - List/ToDoCell.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoCell.swift
+//  Guided Project - List
+//
+//  Created by Student on 16/09/25.
+//
+
+import UIKit
+protocol ToDoCellDelegate: AnyObject {
+func checkmarkTapped (sender: ToDoCell)
+}
+class ToDoCell: UITableViewCell {
+    @IBOutlet weak var iscompletebutton: UIButton!
+    @IBOutlet weak var titlelabel: UILabel!
+    weak var delegate: ToDoCellDelegate?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func completebuttontapped(_ sender: UIButton) {
+        delegate?.checkmarkTapped(sender: self)
+    }
+}
